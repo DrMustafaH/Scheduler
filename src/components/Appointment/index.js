22e8a0fa41e0e11c3de42e6206a807369f5080d2
DrMustafaH <mustafaharith@gmail.com>
2021-02-15 23:36:52 -0800

    Create appointment and implement Header and Empty

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "components/Appointment/style.scss"
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  )
+}
