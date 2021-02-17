d495c04bef8bb7e85686ce6cf96e37361657f558
DrMustafaH <mustafaharith@gmail.com>
2021-02-17 12:17:20 -0800

    Implement selectors helper

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  const outputArr = [];
+
+  state.days.forEach(singleday => {
+    if (day === singleday.name) {
+      singleday.appointments.forEach(e => {
+        const appointementKeys = Object.keys(state.appointments)
+        const filteredArr = appointementKeys.filter(appointmentKey => Number(appointmentKey) === e)
+        filteredArr.forEach(key => {
+          let stringKey = key.toString()
+          let objects = state.appointments[stringKey]
+          outputArr.push(objects)
+        });
+      })
+    }
+  });
+  return outputArr;
+}
