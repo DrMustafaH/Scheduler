dadcc9d7bea29e0b347e8ef75738e1442532a890
DrMustafaH <mustafaharith@gmail.com>
2021-02-21 11:13:18 -0800

    Try prop.type testing

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -26,13 +26,14 @@ const ERROR_DELETE = "ERROR_DELETE";
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
-  );
+    );
+    
+    function save(name, interviewer, type) {
+      const interview = {
+        student: name,
+        interviewer
+      };
 
-  function save(name, interviewer, type) {
-    const interview = {
-      student: name,
-      interviewer
-    };
     transition(SAVING)
     props.bookInterview(props.id, interview, type).then(() => {
       transition(SHOW)
